174b08fd13a7782645263c42cd3c7ffcce2b25cb
dadaa <dadaaism@gmail.com>
2016-11-16 19:54:12 +0900

    fixed chirimen-oh/any-issues#144

--- rockdev-rk3066/mkupdate.sh
+++ rockdev-rk3066/mkupdate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
 ./afptool -pack ./ Image/update.img
-./rkImageMaker -RK30 RK30xxLoader(L)_V2.09.bin Image/update.img update.img -os_type:androidos
+./rkImageMaker -RK30 "RK30xxLoader(L)_V2.09.bin" Image/update.img update.img -os_type:androidos
 rm Image/update.img
